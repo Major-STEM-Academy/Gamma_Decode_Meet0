fa8e639699aa46cf0d8bee037149b7f1ab8a1690
Chirsty222 <41651@students.ausd.net>
2025-11-21 19:07:47 -0800

    StarterBotAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/StarterBotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/StarterBotAuto.java
@@ -1,265 +1,119 @@
-package org.firstinspires.ftc.teamcode.auto;
-
-import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+@Autonomous(name = "StarterBotAuto3Ball", group = "StarterBot")
+public class StarterBotAuto extends LinearOpMode {
 
-@Autonomous(name="StarterBotAuto3Ball", group="StarterBot")
-public class StarterBotAuto extends OpMode {
+    private DcMotorEx launcher;
+    private CRServo leftFeeder;
+    private CRServo rightFeeder;
 
-    // Feeder and launcher timing
-    final double FEED_TIME = 0.2;             // seconds to run feeder per ball
-    final double TIME_BETWEEN_SHOTS = 1.0;    // seconds between shots
-  final double LAUNCHER_TARGET_VELOCITY = 1750;      // spin-up target
-      final double LAUNCHER_MIN_VELOCITY = 1650;
+    private DcMotorEx leftDrive;
+    private DcMotorEx rightDrive;
 
-    // Drive constants
-    final double DRIVE_SPEED = 0.5;
-    final double ROTATE_SPEED = 0.2;
-    final double WHEEL_DIAMETER_MM = 96;
-    final double ENCODER_TICKS_PER_REV = 537.7;
-    final double TICKS_PER_MM = (ENCODER_TICKS_PER_REV / (WHEEL_DIAMETER_MM * Math.PI));
-    final double TRACK_WIDTH_MM = 404;
+    // Launcher speeds
+    final double LAUNCHER_VELOCITY = 1750;      // max spin-up
+    final double    LAUNCHER_MIN_VELOCITY = 1650;  // must reach this before feeding
 
-    int shotsToFire = 3; // Fire 3 balls
+    // Feed timing
+    final double FEED_TIME = 0.5;
+    final double STOP_SPEED = 0.0;
 
-    double robotRotationAngle = 45;
+    int shotsToFire = 1;  // 1 burst of 3 balls
 
-    private ElapsedTime shotTimer = new ElapsedTime();
-    private ElapsedTime feederTimer = new ElapsedTime();
-    private ElapsedTime driveTimer = new ElapsedTime();
-
-    private DcMotor leftFrontDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightBackDrive = null;
-    private DcMotorEx launcher = null;
-    private CRServo leftFeeder = null;
-    private CRServo rightFeeder = null;
-
-    private enum LaunchState {
+    enum LaunchState {
         IDLE,
-        PREPARE,
-        LAUNCH,
+        SPIN_UP,
+        LAUNCH
     }
-    private LaunchState launchState;
 
-    private enum AutonomousState {
-        LAUNCH,
-        WAIT_FOR_LAUNCH,
-        DRIVING_AWAY_FROM_GOAL,
-        ROTATING,
-        DRIVING_OFF_LINE,
-        COMPLETE;
-    }
-    private AutonomousState autonomousState;
-
-    private enum Alliance {
-        RED,
-        BLUE;
-    }
-    private Alliance alliance = Alliance.RED;
+    private LaunchState launchState = LaunchState.IDLE;
+    private ElapsedTime feederTimer = new ElapsedTime();
 
     @Override
-    public void init() {
-        autonomousState = AutonomousState.LAUNCH;
-        launchState = LaunchState.IDLE;
+    public void runOpMode() throws InterruptedException {
 
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "motorfl");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "motorfr");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "motorbl");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "motorbr");
-        launcher = hardwareMap.get(DcMotorEx.class,"hogback");
-        leftFeeder = hardwareMap.get(CRServo.class, "flyWheell");
-        rightFeeder = hardwareMap.get(CRServo.class, "flyWheelr");
+        // Hardware initialization
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        leftFeeder = hardwareMap.get(CRServo.class, "leftFeeder");
+        rightFeeder = hardwareMap.get(CRServo.class, "rightFeeder");
 
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        launcher.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive = hardwareMap.get(DcMotorEx.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotorEx.class, "rightDrive");
 
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFeeder.setPower(STOP_SPEED);
+        rightFeeder.setPower(STOP_SPEED);
 
-        leftFrontDrive.setZeroPowerBehavior(BRAKE);
-        rightFrontDrive.setZeroPowerBehavior(BRAKE);
-        leftBackDrive.setZeroPowerBehavior(BRAKE);
-        rightBackDrive.setZeroPowerBehavior(BRAKE);
-        launcher.setZeroPowerBehavior(BRAKE);
+        launcher.setDirection(DcMotorEx.Direction.REVERSE);
 
-        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300,0,0,10));
+        leftDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorEx.Direction.FORWARD);
 
-        leftFeeder.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        telemetry.addData("Status", "Initialized");
-    }
-
-    @Override
-    public void init_loop() {
-        leftFeeder.setPower(0);
-        rightFeeder.setPower(0);
-
-        if (gamepad1.b) alliance = Alliance.RED;
-        else if (gamepad1.x) alliance = Alliance.BLUE;
-
-        telemetry.addData("Press X", "for BLUE");
-        telemetry.addData("Press B", "for RED");
-        telemetry.addData("Selected Alliance", alliance);
-    }
-
-    @Override
-    public void loop() {
-        switch (autonomousState){
-            case LAUNCH:
-                launch(true);
-                autonomousState = AutonomousState.WAIT_FOR_LAUNCH;
-                break;
-
-            case WAIT_FOR_LAUNCH:
-                if(launch(false)) {
-                    shotsToFire -= 1;
-                    if(shotsToFire > 0) {
-                        autonomousState = AutonomousState.LAUNCH;
-                    } else {
-                        launcher.setVelocity(0);
-                        autonomousState = AutonomousState.DRIVING_AWAY_FROM_GOAL;
-                    }
-                }
-                break;
-
-            case DRIVING_AWAY_FROM_GOAL:
-                if(drive(DRIVE_SPEED, -4, DistanceUnit.INCH, 1)){
-                    leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    autonomousState = AutonomousState.ROTATING;
-                }
-                break;
-
-            case ROTATING:
-                robotRotationAngle = (alliance == Alliance.RED) ? 45 : -45;
-                if(rotate(ROTATE_SPEED, robotRotationAngle, AngleUnit.DEGREES,1)){
-                    leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                    autonomousState = AutonomousState.DRIVING_OFF_LINE;
-                }
-                break;
-
-            case DRIVING_OFF_LINE:
-                if(drive(DRIVE_SPEED, -26, DistanceUnit.INCH, 1)){
-                    autonomousState = AutonomousState.COMPLETE;
-                }
-                break;
-        }
-
-        telemetry.addData("AutoState", autonomousState);
-        telemetry.addData("LauncherState", launchState);
+        telemetry.addLine("Initialized - READY");
         telemetry.update();
-    }
 
-    boolean launch(boolean shotRequested){
-        switch (launchState) {
-            case IDLE:
-                if (shotRequested) {
-                    launchState = LaunchState.PREPARE;
-                    shotTimer.reset();
-                }
-                break;
+        waitForStart();
 
-            case PREPARE:
-                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
-                if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY){
-                    launchState = LaunchState.LAUNCH;
-                    leftFeeder.setPower(1);
-                    rightFeeder.setPower(1);
-                    feederTimer.reset();
-                }
-                break;
+        // *********** MOVE TO GOAL ***********
+        driveForward(0.4, 1.2);  // power, time (seconds)
+        sleep(300); // settle the robot
 
-            case LAUNCH:
-                if (feederTimer.seconds() > FEED_TIME) {
-                    leftFeeder.setPower(0);
-                    rightFeeder.setPower(0);
-                    if(shotTimer.seconds() > TIME_BETWEEN_SHOTS){
-                        launchState = LaunchState.IDLE;
-                        return true;
+        // *********** START SHOOTER ***********
+        launchState = LaunchState.SPIN_UP;
+
+        while (opModeIsActive()) {
+
+            switch (launchState) {
+
+                case SPIN_UP:
+                    launcher.setVelocity(LAUNCHER_VELOCITY);
+
+                    if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY) {
+                        launchState = LaunchState.LAUNCH;
+
+                        leftFeeder.setPower(1);
+                        rightFeeder.setPower(1);
+                        feederTimer.reset();
                     }
-                }
-                break;
+                    break;
+
+                case LAUNCH:
+                    if (feederTimer.seconds() > FEED_TIME) {
+
+                        leftFeeder.setPower(STOP_SPEED);
+                        rightFeeder.setPower(STOP_SPEED);
+
+                        shotsToFire--;
+
+                        if (shotsToFire <= 0) {
+                            launcher.setPower(0);
+                            launchState = LaunchState.IDLE;
+                        }
+                    }
+                    break;
+
+                case IDLE:
+                    launcher.setPower(0);
+                    break;
+            }
+
+            telemetry.addData("Launcher Velocity", launcher.getVelocity());
+            telemetry.addData("State", launchState);
+            telemetry.update();
         }
-        return false;
     }
 
-    boolean drive(double speed, double distance, DistanceUnit distanceUnit, double holdSeconds) {
-        final double TOLERANCE_MM = 10;
-        double targetPosition = (distanceUnit.toMm(distance) * TICKS_PER_MM);
-
-        leftFrontDrive.setTargetPosition((int) targetPosition);
-        rightFrontDrive.setTargetPosition((int) targetPosition);
-        leftBackDrive.setTargetPosition((int) targetPosition);
-        rightBackDrive.setTargetPosition((int) targetPosition);
-
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftFrontDrive.setPower(speed);
-        rightFrontDrive.setPower(speed);
-        leftBackDrive.setPower(speed);
-        rightBackDrive.setPower(speed);
-
-        if(Math.abs(targetPosition - leftFrontDrive.getCurrentPosition()) > (TOLERANCE_MM * TICKS_PER_MM)){
-            driveTimer.reset();
-        }
-
-        return (driveTimer.seconds() > holdSeconds);
-    }
-
-    boolean rotate(double speed, double angle, AngleUnit angleUnit, double holdSeconds){
-        final double TOLERANCE_MM = 10;
-        double targetMm = angleUnit.toRadians(angle)*(TRACK_WIDTH_MM/2);
-
-        double leftTargetPosition = -(targetMm*TICKS_PER_MM);
-        double rightTargetPosition = targetMm*TICKS_PER_MM;
-
-        leftFrontDrive.setTargetPosition((int) leftTargetPosition);
-        rightFrontDrive.setTargetPosition((int) rightTargetPosition);
-        leftBackDrive.setTargetPosition((int) leftTargetPosition);
-        rightBackDrive.setTargetPosition((int) rightTargetPosition);
-
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        leftFrontDrive.setPower(speed);
-        rightFrontDrive.setPower(speed);
-        leftBackDrive.setPower(speed);
-        rightBackDrive.setPower(speed);
-
-        if((Math.abs(leftTargetPosition - leftFrontDrive.getCurrentPosition())) > (TOLERANCE_MM * TICKS_PER_MM)){
-            driveTimer.reset();
-        }
-
-        return (driveTimer.seconds() > holdSeconds);
+    // *********** DRIVE FORWARD FUNCTION ***********
+    public void driveForward(double power, double timeSeconds) {
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
+        sleep((long)(timeSeconds * 1000));
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
     }
 }
